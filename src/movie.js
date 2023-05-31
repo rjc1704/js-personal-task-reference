f94f8ecb9ac1011861f40ac6f710923125f44b2c
Jecheon Ryu <rjc1704@gmail.com>
2023-05-31 18:30:02 +0900

    refactor: 가독성 향상

--- src/movie.js
+++ src/movie.js
@@ -1,4 +1,36 @@
-const fetchMovieData = async () => {
+export const generateMovieCards = async () => {
+  const movies = await fetchMovieData();
+  const cardList = document.querySelector("#card-list");
+  cardList.innerHTML = movies
+    .map(
+      (movie) => `
+          <li class="movie-card" id=${movie.id}>
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+              <h3 class="movie-title">${movie.title}</h3>
+              <p>${movie.overview}</p>
+              <p>Rating: ${movie.vote_average}</p>
+          </li>`
+    )
+    .join("");
+
+  cardList.addEventListener("click", handleClickCard);
+
+  // 이벤트 위임: 하위요소에서 발생한 이벤트를 상위요소에서 처리하도록 해줍니다.
+  function handleClickCard(event) {
+    const target = event.target;
+    if (target !== cardList) {
+      // 카드 외 영역 클릭 시 무시
+      if (target.matches(".movie-card")) {
+        alert(`영화 id: ${target.id}`);
+      } else {
+        // 카드의 자식 태그 (img, h3, p) 클릭 시 부모의 id로 접근
+        alert(`영화 id: ${target.parentNode.id}`);
+      }
+    }
+  }
+};
+
+async function fetchMovieData() {
   const options = {
     method: "GET",
     headers: {
@@ -13,33 +45,4 @@ const fetchMovieData = async () => {
   );
   const data = await response.json();
   return data.results;
-};
-
-export const generateMovieCards = async () => {
-  const movies = await fetchMovieData();
-  const cardList = document.querySelector("#card-list");
-  cardList.innerHTML = movies
-    .map(
-      (movie) => `
-          <div class="movie-card" id=${movie.id}>
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-              <h3 class="movie-title">${movie.title}</h3>
-              <p>${movie.overview}</p>
-              <p>Rating: ${movie.vote_average}</p>
-          </div>`
-    )
-    .join("");
-
-  // 이벤트 위임: 하위요소에서 발생한 이벤트를 상위요소에서 처리하도록 해줍니다.
-  cardList.addEventListener("click", ({ target }) => {
-    if (target !== cardList) {
-      // 카드 외 영역 클릭 시 무시
-      if (target.matches(".movie-card")) {
-        alert(`영화 id: ${target.id}`);
-      } else {
-        // 카드의 자식 태그 (img, h3, p) 클릭 시 부모의 id로 접근
-        alert(`영화 id: ${target.parentNode.id}`);
-      }
-    }
-  });
-};
+}
